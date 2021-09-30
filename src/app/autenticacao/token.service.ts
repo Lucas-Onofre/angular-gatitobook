65992493d382dd95376d9376f8194427d9b62af3
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2021-09-30 11:46:25 -0300

    adding token service

---
+++ src/app/autenticacao/token.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  retornaToken(){
+    //tenta retornar o item KEY, caso não encontre valor retornará em branco
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  salvaToken(token: string){
+    //é setado o valor de KEY, recebendo o valor que entra no param. token
+    localStorage.setItem(KEY, token)
+  }
+
+  excluiToken(token: string){
+    //limpamos o item KEY, para casos de loadout, etc.
+    localStorage.removeItem(KEY)
+  }
+
+  possuiToken(){
+    //retornamos um valor booleano que verifica se temos ou não um token. (por isso o uso das exclamações duplas)
+    return !!this.retornaToken();
+  }
+}
