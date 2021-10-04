d7ae516c626270ff6aaaf951db01fed6e3a11169
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2021-10-04 12:43:17 -0300

    creating route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LoginGuard } from './autenticacao/login.guard';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -5,12 +7,14 @@ const routes: Routes = [
   {path: '', pathMatch:'full', redirectTo: 'home'},
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
     path: 'animais',
-    loadChildren: () => import('./animais/animais.module').then((m) => m.AnimaisModule)
-  }
+    loadChildren: () => import('./animais/animais.module').then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard],
+  },
 ];
 
 @NgModule({
